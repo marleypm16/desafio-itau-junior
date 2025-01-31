c3ede519b4d1776437d2a46a2c8be83f358b2c37
marleypm16 <yarlempm16@gmail.com>
2025-01-31 16:52:10 -0300

    feat : add validations and delete func

--- src/main/java/com/challange/desafioitau/controller/TransacaoController.java
+++ src/main/java/com/challange/desafioitau/controller/TransacaoController.java
@@ -7,6 +7,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.math.BigDecimal;
+import java.time.OffsetDateTime;
 import java.util.List;
 
 @RestController
@@ -16,7 +18,28 @@ public class TransacaoController {
     private TransacaoService transacaoService;
     @PostMapping
     public  ResponseEntity<Transacao>criarTransacao(@RequestBody Transacao transacao) {
+        if (transacao.getDataHora() == null || transacao.getValor() == null){
+            return  new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+        if (transacao.getDataHora().isAfter(OffsetDateTime.now()) || transacao.getValor().compareTo(BigDecimal.ZERO) < 0 ){
+            return  new ResponseEntity<>(HttpStatus.UNPROCESSABLE_ENTITY);
+        }
+
+
+
         Transacao transacaoSalva = transacaoService.salvarTransacao(transacao);
-        return new ResponseEntity<>(transacaoSalva, HttpStatus.CREATED);
+        return new ResponseEntity<>(transacaoSalva,HttpStatus.CREATED);
+    }
+    //Desenvolvimento
+    @GetMapping
+    public ResponseEntity<List<Transacao>> listarTransacoes(){
+        List<Transacao> transacaos = transacaoService.listarTransacoes();
+        return new ResponseEntity<>(transacaos,HttpStatus.OK);
+    }
+
+    @DeleteMapping
+    public ResponseEntity<Void> deletarTransacao(){
+        transacaoService.deletarTransacao();
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 }
