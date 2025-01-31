2433eb1c602c1a52fb96612d1b3d9e3a4c5c609e
marleypm16 <yarlempm16@gmail.com>
2025-01-31 09:04:34 -0300

    refactor : change controller anotation and change function return

--- src/main/java/com/challange/desafioitau/controller/TransacaoController.java
+++ src/main/java/com/challange/desafioitau/controller/TransacaoController.java
@@ -3,16 +3,20 @@ package com.challange.desafioitau.controller;
 import com.challange.desafioitau.model.Transacao;
 import com.challange.desafioitau.service.TransacaoService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
-@RequestMapping("/transacao")
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/transacao")
 public class TransacaoController {
     @Autowired
     private TransacaoService transacaoService;
-    public void criarTransacao(@RequestBody Transacao transacao) {
-        transacaoService.salvarTransacao(transacao);
+    @PostMapping
+    public  ResponseEntity<Transacao>criarTransacao(@RequestBody Transacao transacao) {
+        Transacao transacaoSalva = transacaoService.salvarTransacao(transacao);
+        return new ResponseEntity<>(transacaoSalva, HttpStatus.CREATED);
     }
 }
